5f351f331f23929652d8753c4bc4303d19c0d9f1
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-04-29 19:34:43 +0300

    reformat exceptions

--- src/main/java/com/orchestrix/user/exception/UserAlreadyExistsException.java
+++ src/main/java/com/orchestrix/user/exception/UserAlreadyExistsException.java
@@ -1,11 +1,11 @@
 package com.orchestrix.user.exception;
 
 public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-
     public UserAlreadyExistsException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
     }
 }
