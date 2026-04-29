d00cde27e8a95cd9ea2ab4ec5156a6bcbb2f5a3b
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-04-29 18:33:58 +0300

    add RegisterRequest dto

---
+++ src/main/java/com/orchestrix/user/dto/RegisterRequest.java
@@ -0,0 +1,27 @@
+package com.orchestrix.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        @Size(min = 8, max = 32,
+                message = "Password must be between 8-32 characters long")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).+$",
+                message = "Password must contain uppercase, lowercase, digit and special character")
+        String password,
+
+        @NotBlank
+        @Size(min = 4,
+                message = "Name must be at least 4 letters long")
+        @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must contain only letters")
+        String name
+) {
+}
