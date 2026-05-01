8535906b0567ec4d2cd29a6a9418cf260594f410
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-05-01 19:28:34 +0300

    fix import in RefreshTokenRepository.java

--- src/main/java/com/orchestrix/repository/RefreshTokenRepository.java
+++ src/main/java/com/orchestrix/repository/RefreshTokenRepository.java
@@ -2,7 +2,7 @@ package com.orchestrix.repository;
 
 import com.orchestrix.entity.RefreshToken;
 import org.springframework.data.repository.query.Param;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
