8527a282761b90ee23e7cdf3d4964766e33c2786
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-05-01 16:57:49 +0300

    add refresh/access tokens into auth controller

--- src/main/java/com/orchestrix/user/controller/AuthController.java
+++ src/main/java/com/orchestrix/user/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.orchestrix.user.controller;
 
+import com.orchestrix.security.AccessTokenResponse;
 import com.orchestrix.security.JwtService;
+import com.orchestrix.security.RefreshRequest;
+import com.orchestrix.security.RefreshTokenService;
 import com.orchestrix.user.dto.AuthResponse;
 import com.orchestrix.user.dto.LoginRequest;
 import com.orchestrix.user.dto.RegisterRequest;
-import com.orchestrix.user.entity.Role;
 import com.orchestrix.user.entity.User;
 import com.orchestrix.user.service.UserService;
 import jakarta.validation.Valid;
@@ -21,25 +23,42 @@ public class AuthController {
 
     private final UserService userService;
     private final JwtService jwtService;
+    private final RefreshTokenService refreshTokenService;
 
-    public AuthController(UserService userService, JwtService jwtService) {
+    public AuthController(UserService userService,
+                          JwtService jwtService,
+                          RefreshTokenService refreshTokenService
+    ) {
         this.userService = userService;
         this.jwtService = jwtService;
+        this.refreshTokenService = refreshTokenService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request) {
-        userService.register(request.email(), request.password(), request.name());
-        String token = jwtService.generateToken(request.email(), Role.USER);
+        User user = userService.register(request.email(), request.password(), request.name());
+        String refreshToken = refreshTokenService.generateRefreshToken(user).getToken();
+        String accessToken = jwtService.generateToken(user.getEmail(), user.getRole());
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new AuthResponse(token, request.email()));
+                .body(new AuthResponse(
+                        request.email(),
+                        refreshToken,
+                        accessToken
+                        )
+                );
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request) {
         User user = userService.login(request.email(), request.password());
-        String token = jwtService.generateToken(user.getEmail(), user.getRole());
+        String refreshToken = refreshTokenService.generateRefreshToken(user).getToken();
+        String accessToken = jwtService.generateToken(user.getEmail(), user.getRole());
         return ResponseEntity.ok()
-                .body(new AuthResponse(token, request.email()));
+                .body(new AuthResponse(
+                        request.email(),
+                        refreshToken,
+                        accessToken
+                        )
+                );
     }
 }
