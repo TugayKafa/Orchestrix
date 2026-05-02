f9eab9180bb5d00dbbd09a8fe3ebd64b2ec05baf
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-05-03 00:12:44 +0300

    fix TestRedisConfig to mock ValueOperations for redis calls

--- src/test/java/com/orchestrix/config/TestRedisConfig.java
+++ src/test/java/com/orchestrix/config/TestRedisConfig.java
@@ -5,6 +5,10 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @TestConfiguration
 public class TestRedisConfig {
@@ -12,6 +16,9 @@ public class TestRedisConfig {
     @Bean
     @Primary
     public StringRedisTemplate stringRedisTemplate() {
-        return Mockito.mock(StringRedisTemplate.class);
+        StringRedisTemplate template = mock(StringRedisTemplate.class);
+        ValueOperations<String, String> valueOps = mock(ValueOperations.class);
+        when(template.opsForValue()).thenReturn(valueOps);
+        return template;
     }
 }
