bed939802fd8b4c686010d6cbb353f32b4bf20ab
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-10 21:44:14 +0300

    add register form component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login'
+import { Register } from './register/register'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Login, Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
