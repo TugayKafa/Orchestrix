deb0d4cdfab827ae86183eae001216c46c3eb75c
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-10 20:53:13 +0300

    Wire login form fields with two-way data binding

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+export class Login {
+  email = '';
+  password = '';
+
+  login() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+}
